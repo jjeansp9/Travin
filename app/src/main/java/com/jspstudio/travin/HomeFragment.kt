344c1a43267566ae05dffecfa20e0f045d0c8466
jjeansp9 <jjeansp9@gmail.com>
2022-11-04 14:57:05 +0900

    update 202211041456

--- app/src/main/java/com/jspstudio/travin/HomeFragment.kt
+++ app/src/main/java/com/jspstudio/travin/HomeFragment.kt
@@ -42,7 +42,14 @@ class HomeFragment : Fragment() {
         }
 
         for(i in 0..20){
-            items.add(HomeItem(R.drawable.ic_profile, "홍길동", "서울특별시 서초구", "1시간 전", R.drawable.paris, R.drawable.ic_favorite, R.drawable.ic_comment, "안녕하세요 이번에 가본 곳은 프랑스 파리입니다. 에펠탑이 보이는 것이 정말 아름답네요. 다음에 또 오고 싶은 곳입니다."))
+            items.add(HomeItem(R.drawable.ic_profile,
+                "홍길동",
+                "서울특별시 서초구",
+                "1시간 전",
+                R.drawable.paris,
+                R.drawable.ic_favorite,
+                R.drawable.ic_comment,
+                "안녕하세요 이번에 가본 곳은 프랑스 파리입니다. 에펠탑이 보이는 것이 정말 아름답네요. 다음에 또 오고 싶은 곳입니다."))
         }
 
 
--- app/src/main/java/com/jspstudio/travin/HomeRecyclerAdapter.kt
+++ app/src/main/java/com/jspstudio/travin/HomeRecyclerAdapter.kt
@@ -26,11 +26,11 @@ class HomeRecyclerAdapter constructor(val context: Context, var homeItems:Mutabl
         holder.binding.tvHomeLocation.text = homeItems[position].homeLocation // 글 작성자 위치
         holder.binding.tvHomeTimeBefore.text = homeItems[position].homeTimeBefore // 글 작성시간
         holder.binding.tvHomeContents.text = homeItems[position].homeContents // 작성자의 글 내용
+        holder.binding.icHomeFavorite.textOff = homeItems[position].homeFavorite.toString() // 작성자의 글 좋아요 버튼
 
         Glide.with(context).load(homeItems[position].homeProfile).into(holder.binding.ivHomeProfile) // 글 작성자 프로필
         Glide.with(context).load(homeItems[position].homePicture).into(holder.binding.ivHomePicture) // 글 작성자가 업로드한 사진
-        Glide.with(context).load(homeItems[position].homeFavorite).into(holder.binding.icHomeFavorite) // 글 좋아요 버튼
-        Glide.with(context).load(homeItems[position].homeComment).into(holder.binding.icHomeComment) // 글 좋아요 버튼
+        Glide.with(context).load(homeItems[position].homeComment).into(holder.binding.icHomeComment) // 댓글달기 버튼
     }
 
     override fun getItemCount(): Int = homeItems.size
